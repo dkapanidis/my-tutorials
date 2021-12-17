377b08b519a1a9d32217b3af99297ed2affff89f
Dimitris Kapanidis <d.kapanidis@gmail.com>
2021-12-17 13:43:11 +0100

    cleanup

--- airflow/day02/dags/declare_dag_with_constructor.py
+++ airflow/day02/dags/declare_dag_with_constructor.py
@@ -6,6 +6,6 @@ from airflow.operators.dummy import DummyOperator
 dag = DAG(
     "declare_dag_with_constructor",
     start_date=datetime(2021, 1, 1),
-    tags=["declare_dag"]
+    tags=["declare_dag"],
 )
 op = DummyOperator(task_id="task", dag=dag)
